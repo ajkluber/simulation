7e727f446cdc595d0e778bfc37be2decf590b96f
Alexander Kluber <ajkluber@rice.edu>
2015-10-05 16:39:50 -0500

    [examples/start_umb.py] Starting to make a useable script

--- examples/start_umb.py
+++ examples/start_umb.py
@@ -5,128 +5,131 @@ import subprocess as sb
 import model_builder as mdb
 import project_tools as pjt
 
-def get_pbs(jobname,queue="serial",email=False,analysis=False):
-    pbs_string = "#!/bin/bash \n"
-    pbs_string +="#PBS -N %s\n" % jobname
-    pbs_string +="#PBS -q %s\n" % queue
-    pbs_string +="#PBS -l nodes=1:ppn=1\n"
-    pbs_string +="#PBS -l walltime=10:00:00\n"
-    if email:
-        pbs_string +="#PBS -M alexkluber@gmail.com\n"
-        pbs_string +="#PBS -m ae\n"
-    pbs_string +="#PBS -V \n\n"
-    pbs_string +="echo 'I ran on:'\n"
-    pbs_string +="cat $PBS_NODEFILE\n"
-    pbs_string +="cd $PBS_O_WORKDIR\n"
-    pbs_string +="mdrun_sbm -s topol_4.5.tpr -maxh 23\n"
-    if analysis:
-        pbs_string +="""g_kuh_sbm -s Native.pdb -f traj.xtc -n native_contacts.ndx -o Q -noshortcut -abscut -cut 0.1
-mv Q.out Q.dat
-g_kuh_sbm -s Native.pdb -f traj.xtc -n native_contacts.ndx -o Qtanh -kappa 5
-mv Qtanh.out Qtanh.dat
-g_energy_sbm -f ener.edr -o energyterms -xvg none << EOF
-Bond
-Angle
-Proper-Dih.
-Gaussian
-Potential
-EOF"""
-    return pbs_string
+import simulation.examples.constant_temp as constT
+import simulation.slurm
+import simulation.mdp
+import simulation.run
 
-def get_rst_pbs(jobname,analysis=False):
-    pbs_string = "#!/bin/bash \n"
-    pbs_string +="#PBS -N %s \n" % jobname
-    pbs_string +="#PBS -q serial \n"
-    pbs_string +="#PBS -l nodes=1:ppn=1 \n"
-    pbs_string +="#PBS -l walltime=24:00:00 \n"
-    pbs_string +="#PBS -V \n\n"
-    pbs_string +="echo 'I ran on:'\n"
-    pbs_string +="cat $PBS_NODEFILE\n"
-    pbs_string +="cd $PBS_O_WORKDIR\n"
-    pbs_string +="mdrun_sbm -s topol_4.5.tpr -cpi state.cpt -maxh 23\n"
-    if analysis:
-        pbs_string +="""g_kuh_sbm -s Native.pdb -f traj.xtc -n native_contacts.ndx -o Q -noshortcut -abscut -cut 0.1
-mv Q.out Q.dat
-g_kuh_sbm -s Native.pdb -f traj.xtc -n native_contacts.ndx -o Qtanh -kappa 5
-mv Qtanh.out Qtanh.dat
-g_energy_sbm -f ener.edr -o energyterms -xvg none << EOF
+def get_umbrella_params_string(pairs,r0,cent_umb,k_umb,kappa,umb_steps):
+    umb_string = "%.4f %.4f %.4f %d %d\n" % (cent_umb,k_umb,kappa,pairs.shape[0],umb_steps)
+    for i in range(pairs.shape[0]):
+        umb_string += "%5d %5d %.10f\n" % (pairs[i,0],pairs[i,1],r0[i])
+    return umb_string
+
+def get_args():
+    parser = argparse.ArgumentParser(description='.')
+    parser.add_argument('--name',
+            type=str,
+            required=True,
+            help='Name of ini file.')
+
+    parser.add_argument('--pairsfile',
+            type=str,
+            required=True,
+            help='Name of contacts file.')
+
+    parser.add_argument('--kappa',
+            type=str,
+            default="Native.pdb",
+            help='Contact functional form. Opt.')
+
+    args = parser.parse_args()
+    return args
+
+def get_run_cmd(kappa,postruncmd=""):
+    runcmd = \
+"""grompp_sbm -n index.ndx -f run.mdp -c conf.gro -p topol.top -o topol_4.5.tpr 
+if [ -e topol_4.5.tpr ]; then  
+    mdrun_sbm -s topol_4.5.tpr -maxh 23.5
+    if [ -e traj.xtc ]; then 
+        g_kuh_sbm -s Native.pdb -f traj.xtc -n native_contacts.ndx -o Qumb -kappa %f
+        mv Qumb.out Qumb.dat
+        g_energy_sbm -f ener.edr -o energyterms -xvg none << EOF
 Bond
 Angle
 Proper-Dih.
 Gaussian
 Potential
-EOF"""
-    return pbs_string
+EOF
+    %s
+    else
+        echo "Something went wrong with simulation!"
+    fi
+else
+    echo "Something went wrong with preparation!"
+fi
+""" % (kappa,postruncmd)
+    return runcmd
+
+
+def get_rst_cmd(kappa,postruncmd="",noappend=False):
+    if noappend:
+        noappend = "-noappend"
+    else:
+        noappend = ""
+    rstcmd = \
+"""  
+mdrun_sbm -s topol_4.5.tpr -cpi state.cpt -maxh 23.5 %s
+if [ -e traj.xtc ]; then 
+    g_kuh_sbm -s Native.pdb -f traj.xtc -n native_contacts.ndx -o Qumb -kappa %f
+    mv Qumb.out Qumb.dat
+    g_energy_sbm -f ener.edr -o energyterms -xvg none << EOF
+Bond
+Angle
+Proper-Dih.
+Gaussian
+Potential
+EOF
+    %s
+else
+    echo "Something went wrong with simulation!"
+fi
+""" % (noappend,kappa,postruncmd)
+    return rstcmd
+
 
 if __name__ == "__main__":
 
-    name = "SH3"
-    contacts = np.loadtxt("SH3.contacts",dtype=int)
+    #args = get_args()
+    #name = args.name
+    #pairs = np.loadtxt(args.pairsfile,dtype=int)
 
+    name = "PDZ"
+    pairs = np.loadtxt("cacb_cutoff_contacts",dtype=int)
+
+    # Needed:
     kappa = 5
     k_umb = 0.1
-    Qcenter = range(20,120,20)
-    n_conts = len(contacts)
-    umb_steps = 20000
+    umb_steps = 2e4
+    
+    T = 120
+    nsteps = 1e8
 
-    T = 130.95
-    nsteps = 100000000
+    # determine the centers of umbrellas
+    umb_centers = range(20,pairs.shape[0],20)
+    n_umbs = len(umb_centers)
 
     model,fitopts = mdb.inputs.load_model(name)
 
-    distances = mdb.models.pdb_parser.get_pairwise_distances(name+".pdb",contacts)
-    body_string = ""
-    for i in range(len(contacts)):
-        body_string += "%5d %5d %.10f\n" % (contacts[i,0],contacts[i,1],distances[i])
+    r0 = mdb.models.pdb_parser.get_pairwise_distances(model.cleanpdb,pairs)
 
-    if not os.path.exists("kumb_%.2f" % k_umb):
-        os.mkdir("kumb_%.2f" % k_umb)
-    os.chdir("kumb_%.2f" % k_umb)
+    dirs = [ "umb_%d" % umb_centers[i] for i in range(n_umbs) ]
+    jobnames = [ "umb_%d" % umb_centers[i] for i in range(n_umbs) ]
+    umbrellafiles = [ {"umbrella_params":get_umbrella_params_string(pairs,r0,umb_centers[i],k_umb,kappa,umb_steps)} for i in range(n_umbs) ]
+    runcmd = get_run_cmd(kappa)
+    rstcmd = get_rst_cmd(kappa)
 
-    print "starting kumb_%.2f" % k_umb
+    mdpfiles = [ pjt.simulation.mdp.constant_temperature(str(T),str(nsteps)) for i in range(n_umbs) ]
+    runslurms = [ simulation.slurm.make_script_string(runcmd,jobnames[i],partition="serial",mem_per_cpu="2G",walltime="00:10:00") for i in range(n_umbs) ]
+    rstslurms = [ simulation.slurm.make_script_string(rstcmd,jobnames[i],partition="serial",mem_per_cpu="2G") for i in range(n_umbs) ]
 
-    umb_last = ""
-    for i in range(len(Qcenter)):
-        Q0 = Qcenter[i]
-        if not os.path.exists(str(Q0)):
-            os.mkdir(str(Q0))
-        os.chdir(str(Q0))
-        print "running ", str(Q0)
+    if not os.path.exists("test_%.2f" % T):
+        os.mkdir("test_%.2f" % T)
+    os.chdir("test_%.2f" % T)
 
-        umb_last += str(Q0) + "\n"
-
-        umb_string = "%.4f %.4f %.4f %d %d\n" % (Q0,k_umb,kappa,n_conts,umb_steps)
-        umb_string += body_string
-        with open("umbrella_params","w") as fout:
-            fout.write(umb_string)
-
-        nvt = pjt.simulation.mdp.constant_temperature(str(T),str(nsteps))
-
-        model.save_simulation_files()
-
-        with open("nvt.mdp","w") as fout:
-            # Molecular dynamics parameters
-            fout.write(nvt)
-
-        with open("prep.out","w") as fout:
-            # Prepare run
-            prep_step = 'grompp_sbm -n index.ndx -f nvt.mdp -c conf.gro -p topol.top -o topol_4.5.tpr '
-            sb.call(prep_step.split(),stdout=fout,stderr=fout)
-
-        with open("run.pbs","w") as fout:
-            fout.write(get_pbs(name+"umb"+str(Q0),analysis=True))
-
-        with open("rst.pbs","w") as fout:
-            fout.write(get_rst_pbs(name+"umb"+str(Q0),analysis=True))
-
-        with open("sim.out","w") as fout:
-            # Submit simulation 
-            qsub = "qsub run.pbs"
-            sb.call(qsub.split(),stdout=fout,stderr=fout)
-
-        os.chdir("..")
+    simulation.run.submit_simulations(model.save_simulation_files,dirs,mdpfiles,runslurms,rstslurms,morefiles=umbrellafiles)
 
     with open("umbrella_last","w") as fout:
-        fout.write(umb_last)
+        fout.write("\n".join(dirs))
 
     os.chdir("..")
