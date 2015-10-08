a5889e45e945c3299097fc76f2c39b61b14ab95f
Alexander Kluber <alexkluber@gmail.com>
2015-10-07 19:22:50 -0500

    [binned_contacts.py] Refactored.

--- calc/binned_contacts.py
+++ calc/binned_contacts.py
@@ -4,6 +4,43 @@ import numpy as np
 
 import simulation.calc.util as util
 
+def calculate_binned_contacts_vs_q(args):
+    trajsfile = args.trajs
+    function = args.function 
+    coordfile = args.coordfile
+    coordname = coordfile.split(".")[0]
+    bins = args.bins
+    chunksize = args.chunksize
+    topology = args.topology
+    periodic = args.periodic
+ 
+    # Data source
+    trajfiles = [ "%s" % x.rstrip("\n") for x in open(trajsfile,"r").readlines() ]
+    dir = os.path.dirname(trajfiles[0])
+
+    # Parameterize contact-based reaction coordinate
+    pairs, contact_params = util.get_contact_params(dir,args)
+    coord_sources = [  "%s/%s" % (os.path.dirname(trajfiles[i]),coordfile) for i in range(len(trajfiles)) ]
+
+    if not all([ os.path.exists(x) for x in coord_sources ]):
+        # Parameterize contact-based reaction coordinate
+        contact_function = util.get_sum_contact_function(pairs,function,contact_params,periodic=periodic)
+
+        # Calculate contact function over directories
+        contacts = util.calc_coordinate_multiple_trajs(trajfiles,contact_function,topology,chunksize,save_coord_as=args.coordfile,savepath=args.savepath,collect=True)
+    else:
+        # Load precalculated coordinate
+        contacts = [ np.loadtxt(coord_sources[i]) for i in range(len(coord_sources)) ]
+
+    # Parameterize pairwise contact function
+    pairwise_contact_function = util.get_pair_contact_function(pairs,function,contact_params,periodic=periodic)
+
+    # Calculate pairwise contacts over directories 
+    bin_edges,avgqi_by_bin = util.bin_multiple_coordinates_for_multiple_trajs(trajfiles,
+            contacts,pairwise_contact_function,pairs.shape[0],bins,topology,chunksize)
+
+    return bin_edges, avgqi_by_bin
+
 def get_args():
     parser = argparse.ArgumentParser(description='.')
     parser.add_argument('--trajs',
@@ -21,7 +58,7 @@ def get_args():
             required=True,
             help='Contact functional form.')
 
-    parser.add_argument('--n_bins',
+    parser.add_argument('--bins',
             type=int,
             default=40,
             help='Contact functional form.')
@@ -64,38 +101,10 @@ if __name__ == "__main__":
     args = get_args()
 
     trajsfile = args.trajs
-    function = args.function 
     coordfile = args.coordfile
     coordname = coordfile.split(".")[0]
-    n_bins = args.n_bins
-    chunksize = args.chunksize
-    topology = args.topology
-    periodic = args.periodic
  
-    # Data source
-    trajfiles = [ "%s" % x.rstrip("\n") for x in open(trajsfile,"r").readlines() ]
-    dir = os.path.dirname(trajfiles[0])
-
-    # Parameterize contact-based reaction coordinate
-    pairs, contact_params = util.get_contact_params(dir,args)
-    coord_sources = [  "%s/%s" % (os.path.dirname(trajfiles[i]),coordfile) for i in range(len(trajfiles)) ]
-
-    if not all([ os.path.exists(x) for x in coord_sources ]):
-        # Parameterize contact-based reaction coordinate
-        contact_function = util.get_sum_contact_function(pairs,function,contact_params,periodic=periodic)
-
-        # Calculate contact function over directories
-        contacts = util.calc_coordinate_multiple_trajs(trajfiles,contact_function,topology,chunksize,save_coord_as=args.coordfile,savepath=args.savepath,collect=True)
-    else:
-        # Load precalculated coordinate
-        contacts = [ np.loadtxt(coord_sources[i]) for i in range(len(coord_sources)) ]
-
-    # Parameterize pairwise contact function
-    pairwise_contact_function = util.get_pair_contact_function(pairs,function,contact_params,periodic=periodic)
-
-    # Calculate pairwise contacts over directories 
-    bin_edges,avgQi_by_bin = util.bin_multiple_coordinates_for_multiple_trajs(trajfiles,
-            contacts,pairwise_contact_function,pairs.shape[0],n_bins,topology,chunksize)
+    bin_edges, avgqi_by_bin = calculate_binned_contacts_vs_q(args)
 
     cwd = os.getcwd()
     if args.savepath is not None:
@@ -105,7 +114,7 @@ if __name__ == "__main__":
     if not os.path.exists("binned_contacts_vs_%s" % coordname):
         os.mkdir("binned_contacts_vs_%s" % coordname)
     os.chdir("binned_contacts_vs_%s" % coordname)
-    np.savetxt("contacts_vs_bin.dat",avgQi_by_bin)
+    np.savetxt("contacts_vs_bin.dat",avgqi_by_bin)
     np.savetxt("bin_edges.dat",bin_edges)
     os.chdir(cwd)
     print "Took: %.2f" % ((time.time() - starttime)/60.)
