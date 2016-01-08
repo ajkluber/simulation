3a3897baf76c6c3398ae3885804b37e80399d829
Alexander Kluber <ajkluber@rice.edu>
2016-01-08 14:02:34 -0600

    allow for path to params files to be specified

--- calc/scripts/Enn.py
+++ calc/scripts/Enn.py
@@ -3,19 +3,21 @@ import numpy as np
 
 import simulation.calc.observables as observables
 
-def calc_Enn_for_directories(trajfiles):
-    dir = os.path.dirname(trajfiles[0])
-    nat_pairs = np.loadtxt("%s/native_contacts.ndx" % dir, skiprows=1, dtype=int) - 1
+def calc_Enn_for_directories(trajfiles, path_to_params=None):
+    if path_to_params is None:
+        path_to_params = os.path.dirname(trajfiles[0])
+
+    nat_pairs = np.loadtxt("{}/native_contacts.ndx".format(path_to_params), skiprows=1, dtype=int) - 1
     n_native_pairs = nat_pairs.shape[0]
 
-    top = "%s/Native.pdb" % dir
+    top = "{}/Native.pdb".format(path_to_params)
 
-    nn_pair_type = np.loadtxt("%s/pairwise_params" % dir, usecols=(3,), dtype=int)[2*n_native_pairs + 1::2]
-    nn_pairs = np.loadtxt("%s/pairwise_params" % dir, usecols=(0,1), dtype=int)[2*n_native_pairs + 1::2] - 1
-    nn_eps = np.loadtxt("%s/model_params" % dir)[2*n_native_pairs + 1::2]
+    nn_pair_type = np.loadtxt("{}/pairwise_params".format(path_to_params), usecols=(3,), dtype=int)[2*n_native_pairs + 1::2]
+    nn_pairs = np.loadtxt("{}/pairwise_params".format(path_to_params), usecols=(0,1), dtype=int)[2*n_native_pairs + 1::2] - 1
+    nn_eps = np.loadtxt("{}/model_params".format(path_to_params))[2*n_native_pairs + 1::2]
 
     pair_params = []
-    for line in open("%s/pairwise_params" % dir,"r"):
+    for line in open("{}/pairwise_params".format(path_to_params), "r"):
         if line.startswith("#"):
             continue
         else:
