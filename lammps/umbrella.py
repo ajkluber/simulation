e6dc93c8a946483af801f96fbb7e40636a8f45de
Alexander Kluber <ajkluber@rice.edu>
2016-05-06 14:48:09 +0200

    [lammps/umbrella] drafting umbrella sampling in lammps

---
+++ lammps/umbrella.py
@@ -0,0 +1,7 @@
+
+
+def fix_spring(fix_idx, group1_idx, group2_idx, K, r0):
+
+    fix_string = ""
+    #fix_string += "fix ID groupID spring couple groupID2 K x y z r0\n"
+    fix_string += "fix ID groupID spring couple groupID2 K x y z r0\n"
