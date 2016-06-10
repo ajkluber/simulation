6146edefa0a2c24fefffd6b15da55bebac578dad
Alexander Kluber <ajkluber@rice.edu>
2016-06-09 19:28:40 -0500

    [input_script] changed default to dcd file format

--- lammps/awsem.py
+++ lammps/awsem.py
@@ -184,7 +184,8 @@ def get_solvent_barrier():
 1
 0.00 2.04 0.57 0.57 0.36 1.11 1.17 -1.52 0.87 0.67 0.79 1.47 1.03 1.00 -0.10 0.26 0.37 1.21 1.15 0.39\n\n"""
 
-def prep_constant_temp(model, traj, name, T, n_steps, n_steps_out, frag_strength, frag_mem_string,
+def prep_constant_temp(model, traj, name, T, n_steps, n_steps_out,
+                frag_strength, frag_mem_string, trajname="traj.dcd",
                 awsem_other_param_files=["anti_HB", 
                 "anti_NHB", "anti_one", "burial_gamma.dat",  "gamma.dat",
                 "para_HB", "para_one", "uniform.gamma"], 
@@ -223,7 +224,7 @@ def prep_constant_temp(model, traj, name, T, n_steps, n_steps_out, frag_strength
     with open(infile, "w") as fout:
         lammps_in_string = input_script.get_awsem_in_script(T, n_steps,
                                 topfile, seqfile, CA_idxs, CB_idxs, O_idxs,
-                                n_steps_xtc=n_steps_out, 
+                                n_steps_xtc=n_steps_out, trajname=trajname,
                                 extra_group_defs=extra_group_defs,
                                 extra_fix_defs=extra_fix_defs,
                                 integrator="Langevin",
