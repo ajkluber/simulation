033efc7bb0af284f910d707ba944308b0b66f164
Alexander Kluber <alexkluber@gmail.com>
2016-07-19 10:11:42 -0500

    [mbar] allow extrapolation. read npy files

--- calc/mbar/util.py
+++ calc/mbar/util.py
@@ -22,7 +22,7 @@ def get_organized_temps(tempsfile):
 
     return organized_temps
 
-def get_mbar_multi_temp(tempsfile, n_interpolate, engfile="Etot.dat", usecols=(1,)):
+def get_mbar_multi_temp(tempsfile, n_interpolate, n_extrapolate=0, engfile="Etot.dat", usecols=(1,)):
     """Prepare the MBAR object with multiple temperature data
     
     Parameters
@@ -31,6 +31,8 @@ def get_mbar_multi_temp(tempsfile, n_interpolate, engfile="Etot.dat", usecols=(1
         Name of the file that holds the directory names.
     n_interpolate : int
         Number of temperatures to interpolate.
+    n_extrapolate : int
+        Number of temperatures to extrapolate.
     engfile : str, opt.
         Expected filename for total potential energy.
     usecols : tuple
@@ -44,7 +46,9 @@ def get_mbar_multi_temp(tempsfile, n_interpolate, engfile="Etot.dat", usecols=(1
     organized_temps = get_organized_temps(tempsfile)
 
     print "loading energies"
-    E, u_kn, N_k, beta = get_energies_ukn(organized_temps, n_interpolate=n_interpolate, engfile=engfile, usecols=usecols)
+    E, u_kn, N_k, beta = get_energies_ukn(organized_temps,
+            n_interpolate=n_interpolate, n_extrapolate=n_extrapolate,
+            engfile=engfile, usecols=usecols)
 
     f_k = None
     # calculate mbar object
@@ -66,7 +70,7 @@ def get_mbar_multi_temp(tempsfile, n_interpolate, engfile="Etot.dat", usecols=(1
         save_mbar_info(mbar, beta)
     return mbar, beta, E, u_kn, N_k
 
-def get_energies_ukn(organized_temps, n_interpolate=5, engfile="Etot.dat", usecols=(1,)):
+def get_energies_ukn(organized_temps, n_interpolate=5, n_extrapolate=0, engfile="Etot.dat", usecols=(1,), dT=0.2):
     """Get energies from directories and interpolate"""
 
     sorted_temps = organized_temps.keys()
@@ -76,12 +80,22 @@ def get_energies_ukn(organized_temps, n_interpolate=5, engfile="Etot.dat", useco
     N_k = []
     beta = []
     full_temps = []
+    #dT = sorted_temps[1] - sorted_temps[0]
+    # extrapolate below sampled temperatures
+    for i in range(n_extrapolate):
+        extrap_T = sorted_temps[0] + (i - n_extrapolate)*dT
+        beta.append(1./(KB*extrap_T))
+        N_k.append(0)
+
     for i in range(len(sorted_temps)):
         T = sorted_temps[i]
         dirs_for_temp = organized_temps[T]
         temp_N = 0
         for n in range(len(dirs_for_temp)):
-            E = np.loadtxt("{}/{}".format(dirs_for_temp[n], engfile), usecols=usecols)
+            if engfile.endswith("npy"):
+                E = np.load("{}/{}".format(dirs_for_temp[n], engfile))
+            else:
+                E = np.loadtxt("{}/{}".format(dirs_for_temp[n], engfile), usecols=usecols)
             E_sims.append(E)
             temp_N += len(E)
 
@@ -94,6 +108,12 @@ def get_energies_ukn(organized_temps, n_interpolate=5, engfile="Etot.dat", useco
             for k in range(n_interpolate):
                 beta.append(1./(KB*interp_T[k]))
                 N_k.append(0)
+
+    # extrapolate above sampled temperatures
+    for i in range(n_extrapolate):
+        extrap_T = sorted_temps[-1] + (i + 1)*dT
+        beta.append(1./(KB*extrap_T))
+        N_k.append(0)
 
     E = np.concatenate(E_sims)
     N_k = np.array(N_k)
