20f023033bca120bf7e64f4b0f55a7c41912dda8
Alexander Kluber <alexkluber@gmail.com>
2016-08-03 13:50:11 -0500

    [scripts] updated scripts for q and TPT

---
+++ calc/scripts/calcTPT.py
@@ -0,0 +1,101 @@
+import os
+import glob
+import numpy as np
+
+import simulation.calc.pmfutil as pmfutil
+import simulation.calc.transits as transits
+import simulation.calc.util as util
+
+if __name__ == "__main__":
+    coordfile = "Qtanh_0_05.npy"
+    coordname = coordfile.split(".")[0]
+    #coordfile = "qtanh.npy"
+
+    # get directories
+    tempdirs = glob.glob("T_*_1") + glob.glob("T_*_2") + glob.glob("T_*_3")
+    organized_temps = util.get_organized_temps(temperature_dirs=tempdirs)
+    T = organized_temps.keys()
+    T.sort()
+
+    # determine which temperature is closest to the folding temperature
+    os.chdir(coordname + "_profile")
+    if not (os.path.exists("T_used") and os.path.exists("minima.dat")):
+        dF_min_idx = np.argmin([ (float(open("T_{}_stab.dat".format(x)).read()))**2 for x in T ])
+        T_used = T[dF_min_idx] 
+        mid_bin = np.loadtxt("T_{}_mid_bin.dat".format(T_used))
+        Fdata = np.loadtxt("T_{}_F.dat".format(T_used))
+        xinterp, F = pmfutil.interpolate_profile(mid_bin, Fdata)
+        minidx, maxidx = pmfutil.extrema_from_profile(xinterp, F)
+        np.savetxt("minima.dat", xinterp[minidx])
+        with open("T_used.dat", "w") as fout:
+            fout.write(str(T_used))
+        U = xinterp[minidx].min()
+        N = xinterp[minidx].max()
+    else:
+        with open("T_used", "r") as fin:
+            T_used = float(fin.read())
+        minima = np.loadtxt("minima.dat")
+        U = minima.min()
+        N = minima.max()
+
+    os.chdir("..")
+
+    dirs = organized_temps[T_used]
+
+    # calculate transit time between minima for each trajectory.
+    all_dwellsU = []
+    all_dwellsN = []
+    all_transitsUN = []
+    all_transitsNU = []
+    for i in range(len(dirs)):
+        print dirs[i]
+        os.chdir(dirs[i])
+        if coordfile.endswith(".npy"):
+            x = np.load(coordfile)
+        else:
+            x = np.loadtxt(coordfile)
+        print x.shape
+        dtraj = np.zeros(x.shape[0], int)
+        dtraj[x <= U] = 0
+        dtraj[x >= N] = 2
+        dtraj[(x > U) & (x < N)] = 1
+
+        # calculate transits
+        dwellsU, dwellsN, transitsUN, transitsNU = transits.partition_dtraj(dtraj, 0, 2)
+
+        os.chdir("..")
+
+        all_dwellsU.append(dwellsU)
+        all_dwellsN.append(dwellsN)
+        all_transitsUN.append(transitsUN)
+        all_transitsNU.append(transitsNU)
+
+    folding_time = np.concatenate([ all_dwellsU[i][:,1] + all_transitsUN[i][:,1] for i in range(len(all_dwellsU)) ])
+    unfolding_time = np.concatenate([ all_dwellsN[i][:,1] + all_transitsNU[i][:,1] for i in range(len(all_dwellsN)) ])
+
+    forward_transit_time = np.concatenate([ all_transitsUN[i][:,1] for i in range(len(all_transitsUN)) ])
+    backward_transit_time = np.concatenate([ all_transitsNU[i][:,1] for i in range(len(all_transitsNU)) ])
+
+    # Save transit times
+    if not os.path.exists("%s_transit_time" % coordname): 
+        os.mkdir("%s_transit_time" % coordname)
+    os.chdir("%s_transit_time" % coordname)
+    with open("forward_mean","w") as fout:
+        fout.write("%.2f" % np.mean(forward_transit_time))
+    with open("backward_mean","w") as fout:
+        fout.write("%.2f" % np.mean(backward_transit_time))
+    np.savetxt("forward_transit_times.dat", forward_transit_time, fmt="%5d")
+    np.savetxt("backward_transit_times.dat", backward_transit_time, fmt="%5d")
+    os.chdir("..")
+
+    # Save folding times
+    if not os.path.exists("%s_folding_time" % coordname): 
+        os.mkdir("%s_folding_time" % coordname)
+    os.chdir("%s_folding_time" % coordname)
+    with open("folding_mean","w") as fout:
+        fout.write("%.2f" % np.mean(folding_time))
+    with open("unfolding_mean","w") as fout:
+        fout.write("%.2f" % np.mean(unfolding_time))
+    np.savetxt("folding_times.dat", folding_time, fmt="%5d")
+    np.savetxt("unfolding_times.dat", unfolding_time, fmt="%5d")
+    os.chdir("..")
---
+++ calc/scripts/calcq.py
@@ -0,0 +1,83 @@
+import os
+import glob
+import argparse
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+import mdtraj as md
+
+import simulation.calc.observables as observables
+import simulation.calc.pmfutil as pmfutil
+import simulation.calc.util as util
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name")
+    args = parser.parse_args()
+
+    name = args.name
+
+    trajname = "traj.xtc"
+    topname = "ref.pdb"
+    coordfile = "Qtanh_0_05.npy"
+    coordname = coordfile.split(".")[0]
+    recalculate = True
+
+    # get directories
+    tempdirs = glob.glob("T_*_1") + glob.glob("T_*_2") + glob.glob("T_*_3")
+    organized_temps = util.get_organized_temps(temperature_dirs=tempdirs)
+    T = organized_temps.keys()
+    T.sort()
+    topfile = tempdirs[0] + "/" + topname
+
+    trajfiles = []
+    for i in range(len(T)):
+        trajfiles.append([ x + "/" + trajname for x in organized_temps[T[i]] ])
+
+    # parameterize native contacts function
+    pairs = np.loadtxt(name + ".contacts", dtype=int) - 1
+    if pairs.shape[1] == 4:
+        pairs = pairs[:,(1,3)]
+    n_native_pairs = len(pairs)
+    
+    r0 = md.compute_distances(md.load(topfile), pairs)
+    r0_cont = r0 + 0.1
+    widths = 0.05*np.ones(n_native_pairs)
+    qtanhsum_obs = observables.TanhContactSum(topfile, pairs, r0_cont, widths, periodic=True)
+
+    for i in range(len(T)):
+        print T[i]
+        qtanh_files = [ "{}/{}".format(os.path.dirname(x),coordfile) for x in trajfiles[i] ]
+        qtanh_files_exist = [ os.path.exists(x) for x in qtanh_files ]
+        # this should work except I am experiencing a strange NaN error.
+        if np.all(qtanh_files_exist) and not recalculate:  
+            qtanh = [ np.load(x) for x in qtanh_files ]
+        else:
+            qtanh = []
+            for n in range(len(trajfiles[i])):
+                traj = trajfiles[i][n]
+                qtanh_temp = np.array(qtanhsum_obs.map(md.load(traj, top=topfile)))
+                np.save(os.path.dirname(traj) + "/" + coordfile, qtanh_temp)
+                qtanh.append(qtanh_temp)
+
+        # calculate free energy profile and state definitions
+        mid_bin, Fdata = pmfutil.pmf1D(np.concatenate(qtanh), bins=40)
+        #plt.plot(mid_bin, Fdata, label=str(T[i]))
+
+        if not os.path.exists(coordname + "_profile"):
+            os.mkdir(coordname + "_profile")
+        os.chdir(coordname + "_profile")
+        np.savetxt("T_{}_mid_bin.dat".format(T[i]), mid_bin)
+        np.savetxt("T_{}_F.dat".format(T[i]), Fdata)
+
+        xinterp, F = pmfutil.interpolate_profile(mid_bin, Fdata)
+        minidx, maxidx = pmfutil.extrema_from_profile(xinterp, F)
+
+        F_fit = F(xinterp)
+        with open("T_{}_stab.dat".format(T[i]), "w") as fout:
+            fout.write(str(F_fit[minidx[1]] - F_fit[minidx[0]]))
+         
+        os.chdir("..")
+
