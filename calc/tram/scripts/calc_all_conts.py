1be344e58f2b9b159f894ab5f34d5b07dfeaa55f
Alexander Kluber <alexkluber@gmail.com>
2016-08-22 17:17:17 -0500

    [scripts/calc_all_conts.py] calculate non-native contacts versus loop length. WORKS

--- calc/tram/scripts/calc_all_conts.py
+++ calc/tram/scripts/calc_all_conts.py
@@ -1,30 +1,25 @@
 import os
 import argparse
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-cmap = cm.get_cmap("viridis")
 
 import mdtraj as md
 import pyemma.coordinates as coor
-import pyemma.msm as msm
 
 import simulation.calc.tram.util as util
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("name")
-    parser.add_argument("n_native_pairs", type=int)
     parser.add_argument("pairwise_params_file")
+    parser.add_argument("n_native_pairs", type=int)
 
     args = parser.parse_args()
-    name = args.name
     n_native_pairs = args.n_native_pairs
     pairwise_file = args.pairwise_params_file
 
     n_qbins = 40
     topname = "ref.pdb"
     trajname = "traj.xtc"
+    color_idxs = np.arange(n_qbins)/float(n_qbins)
 
     with open("Qtanh_0_05_profile/T_used.dat","r") as fin:
         T = float(fin.read())
@@ -35,16 +30,10 @@ if __name__ == "__main__":
 
     qtrajs = [ np.load(x + "/Qtanh_0_05.npy") for x in tempdirs ]
 
-    #topfile = topname
-    #trajfiles = [ trajname ]
-
     # add features
     non_feat = coor.featurizer(topfile)
     non_feat, feature_info = util.sbm_contact_features(non_feat, pairwise_file, n_native_pairs, skip_nn=1, nonnative_only=True)
     n_res = non_feat.topology.n_residues
-
-    #nat_feat = coor.featurizer(topfile)
-    #nat_feat, feature_info = util.sbm_contact_features(nat_feat, pairwise_file, n_native_pairs, skip_nn=1, nonnative_only=True)
 
     n_non_dim = feature_info['dim']
     non_pairs = feature_info['pairs']
@@ -53,7 +42,10 @@ if __name__ == "__main__":
     P_q *= (qbins[1] - qbins[0])
     q_mid_bin = 0.5*(qbins[:-1] + qbins[1:])
 
-    # calculate contacts
+    loop_length = np.arange(3, n_res)
+    X, Y = np.meshgrid(qbins, loop_length)
+
+    # calculate contacts as a function of Q.
     Ai_vs_Q = np.zeros((n_qbins, n_non_dim), float)
     bin_counts = np.zeros(n_qbins)
     for i in range(len(trajfiles)):
@@ -70,15 +62,9 @@ if __name__ == "__main__":
 
     not0 = np.nonzero(bin_counts)
     Ai_vs_Q[not0] = (Ai_vs_Q[not0].T/bin_counts[not0]).T
+    A_vs_Q = np.mean(Ai_vs_Q, axis=1)
 
-    if not os.path.exists("Ai_vs_Qtanh_0_05"):
-        os.mkdir("Ai_vs_Qtanh_0_05")
-    os.chdir("Ai_vs_Qtanh_0_05")
-
-
-    #C_vs_Q = np.zeros((n_qbins, n_res, n_res))
     A_vs_loop_vs_Q = np.zeros((n_qbins, n_res - 3))
-    color_idxs = np.arange(n_qbins)/float(n_qbins)
     for n in range(n_qbins):
         # contact map
         C = np.nan*np.zeros((n_res, n_res))
@@ -88,42 +74,39 @@ if __name__ == "__main__":
 
         # average non-native contacts versus loop length
         A_vs_loop = np.array([ np.ma.mean(C_ma.diagonal(-i)) for i in range(3, n_res) ])
-        loop_length = np.arange(3, n_res)
         A_vs_loop_vs_Q[n] = A_vs_loop
-
-        log_C_ma = np.ma.log(C_ma)
-            
-        # plot contact map
-        #plt.figure(n + 2)
-        #plt.pcolormesh(log_C_ma, vmin=-9, vmax=0, cmap='viridis')
-        #plt.xlabel("residue i")
-        #plt.xlabel("residue j")
-        #plt.title("Contact frequency $Q = {}$".format(q_mid_bin[9]))
-        #cbar = plt.colorbar()
-        #cbar.set_label("$\\log Q_{ij}$")
-        ##plt.savefig("")
-
-        #plt.figure()
-        plt.plot(loop_length, A_vs_loop, color=cmap(color_idxs[n]))
-        #plt.semilogy()
-        plt.xlabel("Loop length")
-        #plt.ylabel("$\\log A$")
-        plt.ylabel("Average non-native contacts")
-
-
     A_vs_loop_vs_Q_ma = np.ma.array(A_vs_loop_vs_Q, mask=np.isnan(A_vs_loop_vs_Q))
 
-    plt.figure()
-    plt.pcolormesh(A_vs_loop_vs_Q_ma.T, cmap="viridis")
-    plt.xlabel("Q")
-    plt.ylabel("Loop length")
-    plt.colorbar()
+    #import matplotlib.pyplot as plt
+    #import matplotlib.cm as cm
+    #cmap = cm.get_cmap("viridis")
+    # plot loop length versus Q
+    #plt.figure()
+    #for n in range(n_qbins):
+    #    plt.plot(loop_length, A_vs_loop_vs_Q[n], color=cmap(color_idxs[n]))
+    #    plt.semilogy()
+    #    plt.xlabel("Loop length")
+    #    plt.ylabel("Average non-native contacts")
 
-    plt.show()
+    # Plot loop length versus
+    #plt.figure()
+    #plt.pcolormesh(X, Y, A_vs_loop_vs_Q_ma.T, cmap="viridis")
+    #plt.xlabel("Q")
+    #plt.ylabel("Loop length")
+    #cbar = plt.colorbar()
+    #cbar.set_label("A")
+    #plt.show()
+
+    if not os.path.exists("Ai_vs_Qtanh_0_05"):
+        os.mkdir("Ai_vs_Qtanh_0_05")
+    os.chdir("Ai_vs_Qtanh_0_05")
 
     # save
-    #np.savetxt("nonnative_pairs.dat", non_pairs, fmt="%4d") 
-    #np.savetxt("loop_length.dat", loop_length, fmt="%4d")
-    #np.savetxt("A_vs_loop.dat", A_vs_loop)
+    np.savetxt("nonnative_pairs.dat", non_pairs, fmt="%4d") 
+    np.savetxt("loop_length.dat", loop_length, fmt="%4d")
+    A_vs_loop_vs_Q_ma.dump("A_vs_loop_vs_Q.npy")
+    np.save("Ai_vs_Q.npy", Ai_vs_Q)
+    np.save("A_vs_Q.npy", A_vs_Q)
 
     os.chdir("..")
+
