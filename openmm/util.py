22c41e4658d29b01ada03d06857805579d6f53d5
Alexander Kluber <ajkluber@rice.edu>
2018-09-21 16:01:08 -0500

    deprecated function

--- openmm/util.py
+++ openmm/util.py
@@ -25,39 +25,41 @@ def output_filenames(name, traj_idx):
     final_state_name = name + "_final_state_{}.xml".format(traj_idx)
     return min_name, log_name, traj_name, final_state_name 
 
-def get_starting_coordinates(name, traj_idx):
-
-    # get initial configuration
-    if traj_idx == 1:
-        print "starting new simulation from: " + name + "_min.pdb"
-        pdb = app.PDBFile(name + "_min.pdb")
-        topology = pdb.topology
-        positions = pdb.positions
-        minimize = True
-    else:
-        minimize = False
-        #omm.
-        prev_state_name = name + "_final_state_{}.xml".format(traj_idx - 1)
-        if os.path.exists(prev_state_name):
-            print "extending from " + prev_state_name
-            pdb = app.PDBFile(name + "_min.pdb")
-            topology = pdb.topology
-
-            state = omm.XmlSerializer().deserialize(prev_state_name)
-            positions = state.getPositions()
-        elif os.path.exists(name + "_traj_{}.dcd".format(traj_idx - 1)) and os.path.exists(name + "_min.pdb"):
-            print "extending from final frame of " + name + "_traj_{}.pdb".format(traj_idx - 1)
-            # How to extend from state file if crash?
-            import mdtraj as md
-            traj = md.load(name + "_traj_{}.dcd".format(traj_idx - 1), top=name + "_min.pdb")
-            traj[-1].save_pdb(name + "_fin_{}.pdb".format(traj_idx - 1))
-            pdb = app.PDBFile(name + "_fin_{}.pdb".format(traj_idx - 1))
-            topology = pdb.topology
-            positions = pdb.positions
-        else:
-            raise IOError("No structure to start from!")
-
-    return topology, positions
+#def get_starting_coordinates(name, traj_idx):
+#    ### DEPRECATED
+#
+#    # get initial configuration
+#    if traj_idx == 1:
+#        print "starting new simulation from: " + name + "_min.pdb"
+#        pdb = app.PDBFile(name + "_min.pdb")
+#        topology = pdb.topology
+#        positions = pdb.positions
+#        minimize = True
+#    else:
+#        minimize = False
+#        #omm.
+#        prev_state_name = name + "_final_state_{}.xml".format(traj_idx - 1)
+#        if os.path.exists(prev_state_name):
+#            print "extending from " + prev_state_name
+#            pdb = app.PDBFile(name + "_min.pdb")
+#            topology = pdb.topology
+#
+#            #state = omm.XmlSerializer().deserialize(prev_state_name)
+#            state = omm.XmlSerializer__deserializeState(prev_state_name)
+#            positions = state.getPositions()
+#        elif os.path.exists(name + "_traj_{}.dcd".format(traj_idx - 1)) and os.path.exists(name + "_min.pdb"):
+#            print "extending from final frame of " + name + "_traj_{}.pdb".format(traj_idx - 1)
+#            # How to extend from state file if crash?
+#            import mdtraj as md
+#            traj = md.load(name + "_traj_{}.dcd".format(traj_idx - 1), top=name + "_min.pdb")
+#            traj[-1].save_pdb(name + "_fin_{}.pdb".format(traj_idx - 1))
+#            pdb = app.PDBFile(name + "_fin_{}.pdb".format(traj_idx - 1))
+#            topology = pdb.topology
+#            positions = pdb.positions
+#        else:
+#            raise IOError("No structure to start from!")
+#
+#    return topology, positions
 
 def add_elements(mass_slv, mass_ply):
     # we define our coarse-grain beads as additional elements in OpenMM.
